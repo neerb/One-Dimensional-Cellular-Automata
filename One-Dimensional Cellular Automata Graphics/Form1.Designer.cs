aae666f7c1bd940cd7c57178d6ad0ca50b86dbde
Nathan <31359612+neerb@users.noreply.github.com>
2019-10-16 16:56:40 -0500

    Add files via upload

---
+++ One-Dimensional Cellular Automata Graphics/Form1.Designer.cs
@@ -0,0 +1,79 @@
+﻿namespace One_Dimensional_Cellular_Automata_Graphics
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.button1 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(17, 16);
+            this.pictureBox1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(1003, 812);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(1187, 800);
+            this.button1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(100, 28);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Generate";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ControlDarkDark;
+            this.ClientSize = new System.Drawing.Size(1303, 843);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.pictureBox1);
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ One-Dimensional Cellular Automata Graphics/Form1.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace One_Dimensional_Cellular_Automata_Graphics
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        //int[] ruleSet = { 0, 1, 0, 1, 1, 0, 1, 0 }; //Rule 90
+        //int[] ruleSet = { 0, 0, 1, 1, 1, 1, 1, 0 }; //Rule 62
+        //int[] ruleSet = { 1, 0, 0, 1, 0, 1, 1, 0 }; //Rule 150
+        //int[] ruleSet = { 1, 1, 0, 1, 1, 1, 1, 0 }; //Rule 222
+        //int[] ruleSet = { 1, 1, 1, 1, 1, 0, 1, 0 }; //Rule 250
+        //int[] ruleSet = { 1, 0, 0, 1, 0, 0, 0, 1 }; //Rule 145
+        int[] ruleSet = { 0, 1, 1, 0, 0, 0, 1, 1 }; //Rule 99
+
+
+        int generations = 1000;
+        int size = 500;
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Bitmap b = new Bitmap(1000, 1000);
+            Graphics g = Graphics.FromImage(b);
+
+            Array.Reverse(ruleSet);
+
+            int[] cells = new int[size];
+
+            cells[size / 2] = 1;
+
+            int inc = b.Width / size;
+            int yCoord = 0;
+
+            yCoord += inc;
+            int[] pastArray;
+            for (int gens = 0; gens < generations; gens++)
+            {
+                pastArray = cells;
+                cells = NewArray(pastArray);
+                for (int i = 0; i < cells.Length; i++)
+                {
+                    if(cells[i] == 1)
+                        g.FillRectangle(Brushes.White, new Rectangle(i * inc, yCoord, inc, inc));
+                    else
+                        g.FillRectangle(Brushes.Black, new Rectangle(i * inc, yCoord, inc, inc));
+                }
+
+                yCoord += inc;
+            }
+
+            pictureBox1.Image = b;
+        }
+
+        int[] NewArray(int[] cells)
+        {
+            int[] newCells = new int[size];
+
+            for(int i = 0; i < newCells.Length; i++)
+            {
+                int sum = 0;
+                int degree = 2;
+                for(int x = i - 1; x <= i + 1; x++)
+                {
+                    if (x >= 0 && x < size)
+                    {
+                        if(cells[x] == 1)
+                            sum += (int)Math.Pow(2, degree);
+                    }
+
+                    degree--;
+                }
+
+                newCells[i] = ruleSet[sum];
+            }
+
+            return newCells;
+        }
+    }
+}
